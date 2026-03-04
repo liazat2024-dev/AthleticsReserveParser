40089b837a07aae70621712371aaa3fe89393f5b
liazat2024-dev <liazat2024@gmail.com>
2026-03-04 12:37:36 +0500

    Update reserve_parser.py

--- reserve_parser.py
+++ reserve_parser.py
@@ -123,7 +123,7 @@ def parse_event(event_url):
 
 if __name__ == "__main__":
     # ВСТАВЬ СЮДА ССЫЛКУ НА СТРАНИЦУ СОРЕВНОВАНИЯ
-    EVENT_URL = "ВСТАВЬ_ССЫЛКУ_СЮДА"
+    EVENT_URL = "https://reserve.la55.ru/event/12345"
 
     data = parse_event(EVENT_URL)
 
